90385d5500c582c15c05039c57b7bb1e8aa4e6b6
Geordie Rose <geordie@sanctuary.ai>
2024-10-14 12:34:29 -0700

    added and changed some parameters

--- utils/parameters.py
+++ utils/parameters.py
@@ -3,15 +3,21 @@
 
 class Params(object):
     def __init__(self):
-        self.GRAPH_NUMBER = 3
+        self.GRAPH_NUMBER = 2
         # 1 = 2 vertices
         # 2 = 3 vertices in triangle
         # 3 = 4 vertices, 6 edges
         # 4 = 6 vertices, 1 hexagon, 6 edges
         # 5 = 10 vertices, 15 edges, petersen graph
         # 6 = 16 vertices, 32 edges, non-planar, tesseract
+        self.USE_QC = True
         self.USE_PRECOMPUTED_RESULTS = True
         self.GET_PRECOMPUTED_RESULTS = True
 
-        self.NUM_READS = 10000
+        self.NUM_READS_SA = 1038000
+        self.NUM_READS_QC = 10
         self.EPSILON = 0.5
+        self.ANNEAL_TIME_IN_NS = 5
+        self.SPIN_REVERSAL_TRANSFORMS = 10
+
+        self.USE_MOCK_DWAVE_SAMPLER = True
