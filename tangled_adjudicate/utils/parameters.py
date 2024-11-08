bdfe37e9a5589bc6d27c6d8791d62b557a8c1eaf
Geordie Rose <geordie@sanctuary.ai>
2024-11-08 09:17:57 -0800

    added QC_SOLVER_TO_USE and moved things around a bit

--- tangled_adjudicate/utils/parameters.py
+++ tangled_adjudicate/utils/parameters.py
@@ -3,22 +3,25 @@
 
 class Params(object):
     def __init__(self):
-        self.GRAPH_NUMBER = 3
+        self.GRAPH_NUMBER = 3   # this is the index of the graph to use; defined in /utils/game_graph_properties.py
+        # just a reminder which are which:
         # 1 = 2 vertices
         # 2 = 3 vertices in triangle
         # 3 = 4 vertices, 6 edges
         # 4 = 6 vertices, 1 hexagon, 6 edges
         # 5 = 10 vertices, 15 edges, petersen graph
         # 6 = 16 vertices, 32 edges, non-planar, tesseract
-        self.USE_QC = True
-        self.USE_PRECOMPUTED_RESULTS = True
-        self.GET_PRECOMPUTED_RESULTS = True
 
-        # self.NUM_READS_SA = 10380000   # 2076000 for graph 2
-        self.NUM_READS_SA = 10000   # 5256000 for graph 3
-        self.NUM_READS_QC = 10
+        # this is the boundary between a draw and a win; don't touch this unless you are a wizard doing wizard shit
         self.EPSILON = 0.5
+
+        # this is for simulated annealing
+        self.NUM_READS_SA = 10000
+
+        # These are parameters related to the use of QC hardware, if you're not using QC you can just leave these
+        self.USE_QC = True
+        self.USE_MOCK_DWAVE_SAMPLER = False    # set to True if you want a software version of the hardware (doesn't sample like the HW tho so don't trust it, just for debugging
+        self.QC_SOLVER_TO_USE = 'Advantage2_prototype2.5'   # modify if you want to use a different QC
+        self.NUM_READS_QC = 10
         self.ANNEAL_TIME_IN_NS = 5
         self.SPIN_REVERSAL_TRANSFORMS = 2
-
-        self.USE_MOCK_DWAVE_SAMPLER = False
