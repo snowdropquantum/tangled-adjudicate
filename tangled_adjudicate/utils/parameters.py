1ae04be233ed72731c1f65b170f4c50c8c446f1f
Geordie Rose <greatblueheron@users.noreply.github.com>
2024-11-09 11:26:11 -0800

    changed defaults to graph 2 and USE_QC = False

--- tangled_adjudicate/utils/parameters.py
+++ tangled_adjudicate/utils/parameters.py
@@ -3,7 +3,7 @@
 
 class Params(object):
     def __init__(self):
-        self.GRAPH_NUMBER = 3   # this is the index of the graph to use; defined in /utils/game_graph_properties.py
+        self.GRAPH_NUMBER = 2   # this is the index of the graph to use; defined in /utils/game_graph_properties.py
         # just a reminder which are which:
         # 1 = 2 vertices
         # 2 = 3 vertices in triangle
@@ -19,7 +19,7 @@ class Params(object):
         self.NUM_READS_SA = 10000
 
         # These are parameters related to the use of QC hardware, if you're not using QC you can just leave these
-        self.USE_QC = True
+        self.USE_QC = False
         self.USE_MOCK_DWAVE_SAMPLER = False    # set to True if you want a software version of the hardware (doesn't sample like the HW tho so don't trust it, just for debugging
         self.QC_SOLVER_TO_USE = 'Advantage2_prototype2.5'   # modify if you want to use a different QC
         self.NUM_READS_QC = 10
