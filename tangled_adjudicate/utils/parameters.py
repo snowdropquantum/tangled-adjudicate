fbc0995ae79806a40ac8f4fb57b4b63d86157664
Geordie Rose <geordie@sanctuary.ai>
2024-11-19 10:19:07 -0800

    final default parameters

--- tangled_adjudicate/utils/parameters.py
+++ tangled_adjudicate/utils/parameters.py
@@ -3,7 +3,7 @@
 
 class Params(object):
     def __init__(self):
-        self.GRAPH_NUMBER = 3   # this is the index of the graph to use; defined in /utils/game_graph_properties.py
+        self.GRAPH_NUMBER = 2   # this is the index of the graph to use; defined in /utils/game_graph_properties.py
         # just a reminder which are which:
         # 1 = 2 vertices
         # 2 = 3 vertices in triangle
@@ -12,22 +12,20 @@ class Params(object):
         # 5 = 10 vertices, 15 edges, petersen graph
         # 6 = 16 vertices, 32 edges, non-planar, tesseract
 
-        # this is the boundary between a draw and a win; don't touch this unless you are a wizard doing wizard shit
-        self.EPSILON = 0.5
+        self.EPSILON = 0.5              # this is the boundary between a draw and a win
 
-        # this is for simulated annealing; default is insanely large, you probably don't need this many
-        self.NUM_READS_SA = 10000000
+        self.NUM_READS_SA = 1000        # this is for simulated annealing
 
         # These are parameters related to the use of QC hardware, if you're not using QC you can just leave these
         # The defaults here are no shimming, no gauge transforms, only use M=1 automorphism, and collect a lot of
         # samples (N=1000)
 
-        self.USE_QC = True
-        self.USE_MOCK_DWAVE_SAMPLER = False   # set to True if you want a software version of the hardware (doesn't sample like the HW tho so don't trust it, just for debugging)
+        self.USE_QC = False
+        self.USE_MOCK_DWAVE_SAMPLER = True   # set to True if you want a software version of the hardware (doesn't sample like the HW tho so don't trust it, just for debugging)
         self.QC_SOLVER_TO_USE = 'Advantage2_prototype2.5'   # modify if you want to use a different QC
 
-        self.NUMBER_OF_CHIP_RUNS = 1
-        self.NUM_READS_QC = 1000
+        self.NUMBER_OF_CHIP_RUNS = 1    # this is M
+        self.NUM_READS_QC = 1000        # this is N
         self.ANNEAL_TIME_IN_NS = 5
 
         self.USE_GAUGE_TRANSFORM = False
