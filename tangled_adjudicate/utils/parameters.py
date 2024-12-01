b5f25828ff941e961d7ec95b729fabdcd69d058d
Geordie Rose <geordie@sanctuary.ai>
2024-12-01 11:42:38 -0800

    changed default params; changed USE_QC to False, changed QC solver to 2.6

--- tangled_adjudicate/utils/parameters.py
+++ tangled_adjudicate/utils/parameters.py
@@ -14,15 +14,15 @@ class Params(object):
 
         self.EPSILON = 0.5              # this is the boundary between a draw and a win
 
-        self.NUM_READS_SA = 100000      # this is for simulated annealing
+        self.NUM_READS_SA = 1000        # this is for simulated annealing
 
         # These are parameters related to the use of QC hardware, if you're not using QC you can just leave these
         # The defaults here are no shimming, no gauge transforms, only use M=1 automorphism, and collect a lot of
         # samples (N=1000)
 
-        self.USE_QC = True              # set to False if you just want to use e.g. simulated annealer
+        self.USE_QC = False             # set to False if you just want to use e.g. simulated annealer
         self.USE_MOCK_DWAVE_SAMPLER = False   # set to True if you want a software version of the hardware (doesn't sample like the HW tho so don't trust it, just for debugging)
-        self.QC_SOLVER_TO_USE = 'Advantage2_prototype2.5'   # modify if you want to use a different QC
+        self.QC_SOLVER_TO_USE = 'Advantage2_prototype2.6'   # modify if you want to use a different QC
 
         self.NUMBER_OF_CHIP_RUNS = 1    # this is M
         self.NUM_READS_QC = 1000        # this is N
