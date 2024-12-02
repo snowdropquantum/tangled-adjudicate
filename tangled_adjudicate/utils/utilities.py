9ac5bf2ca53a1ce1c0f5869f983215c905dff665
Geordie Rose <geordie@sanctuary.ai>
2024-12-02 08:20:44 -0800

    added convert_to_erik_game_state_for_adjudication (moved from tangled-agent)

--- tangled_adjudicate/utils/utilities.py
+++ tangled_adjudicate/utils/utilities.py
@@ -83,3 +83,41 @@ def convert_erik_game_state_to_my_game_state(game_state):
     my_state = vertex_state_list + edge_state_list
 
     return my_state
+
+
+def convert_to_erik_game_state_for_adjudication(my_state, number_of_vertices, list_of_edge_tuples):
+
+    my_vertices = my_state[:number_of_vertices]
+    turn_count = 0
+
+    try:
+        player_1_vertex = my_vertices.index(1)
+        turn_count += 1
+    except ValueError:
+        player_1_vertex = -1
+
+    try:
+        player_2_vertex = my_vertices.index(2)
+        turn_count += 1
+    except ValueError:
+        player_2_vertex = -1
+
+    my_edges = my_state[number_of_vertices:]
+
+    turn_count += my_edges.count(1) + my_edges.count(2) + my_edges.count(3)
+
+    # if turn_count is even, it's player 1 (red)'s turn
+    current_player_idx = 1 if turn_count % 2 == 0 else 2
+
+    erik_edges = []
+    for k in range(len(list_of_edge_tuples)):
+        erik_edges.append((list_of_edge_tuples[k][0], list_of_edge_tuples[k][1], my_edges[k]))
+
+    game_state = {'num_nodes': number_of_vertices,
+                  # 'edges': [(0, 1, 3), (0, 2, 1), (0, 3, 3), (1, 2, 1), (1, 3, 3), (2, 3, 1)],
+                  'edges': erik_edges,
+                  'player1_id': 'player1', 'player2_id': 'player2', 'turn_count': turn_count,
+                  'current_player_index': current_player_idx, 'player1_node': player_1_vertex,
+                  'player2_node': player_2_vertex}
+
+    return game_state
