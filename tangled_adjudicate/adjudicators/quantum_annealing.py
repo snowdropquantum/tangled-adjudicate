f7d765f5d14048b05eecadc793814160f9f7013e
Geordie Rose <geordie@sanctuary.ai>
2025-01-30 10:32:19 -0800

    WIP

--- tangled_adjudicate/adjudicators/quantum_annealing.py
+++ tangled_adjudicate/adjudicators/quantum_annealing.py
@@ -133,26 +133,41 @@ class QuantumAnnealingAdjudicator(Adjudicator):
     def _process_embedding(
         self,
         game_state: GameState,
-        automorphism: Dict[int, int]
+        automorphism: Dict[int, int],
+        num_embeddings: int
     ) -> Dict[int, List[int]]:
         """Process embedding with given automorphism.
         
         Args:
             game_state: Current game state
             automorphism: Graph automorphism to apply
+            num_embeddings: number of embeddings to use; default is all of them
             
         Returns:
             Processed embedding mapping
         """
-        inverted_automorphism = {v: k for k, v in automorphism.items()}
+
         num_vertices = game_state['num_nodes']
-        
+
+        inverted_automorphism_to_use = {v: k for k, v in automorphism.items()}  # swaps key <-> values
+
+        permuted_embedding = []
+
+        for each_embedding in self.embeddings[:num_embeddings]:  # each_embedding is like [1093, 1098, 136]; 343 of these for three-vertex graph
+            this_embedding = []
+            for each_vertex in range(num_vertices):  # each_vertex ranges from 0 to 2
+                this_embedding.append(each_embedding[inverted_automorphism_to_use[each_vertex]])
+            permuted_embedding.append(this_embedding)
+
+        # given that permuted_embedding looks like [[1229, 1235, 563], [872, 242, 866], ...]
+        # this next part converts into the format {0: [1229], 1: [1235], 2: [563], 3: [872], 4: [242], 5: [866]}
+
         embedding_map = {}
-        for embedding_idx, embedding in enumerate(self.embeddings):
-            for vertex in range(num_vertices):
-                logical_idx = num_vertices * embedding_idx + vertex
-                physical_qubit = embedding[inverted_automorphism[vertex]]
-                embedding_map[logical_idx] = [physical_qubit]
+
+        for embedding_idx in range(num_embeddings):
+            for each_vertex in range(num_vertices):  # up to 0..1037
+                embedding_map[num_vertices * embedding_idx + each_vertex] = \
+                    [permuted_embedding[embedding_idx][each_vertex]]
                 
         return embedding_map
 
@@ -181,15 +196,210 @@ class QuantumAnnealingAdjudicator(Adjudicator):
         all_samples = None
         indices_of_flips = None
 
+        # set up sampler kwargs
         if self.params.use_mock and self.params.use_shim:
             print('D-Wave mock sampler is not set up to use the shimming process, turn shim off if using mock!')
 
-        # Process each chip run
+        sampler_kwargs =  {
+                'num_reads': self.params.num_reads,
+                'answer_mode': 'raw'
+            }
+
+        if self.params.use_mock:
+            base_sampler = MockDWaveSampler(topology_type='zephyr', topology_shape=[6, 4])
+        else:
+            base_sampler = DWaveSampler(solver=self.params.QC_SOLVER_TO_USE)
+            sampler_kwargs.update({
+                'fast_anneal': True,
+                'annealing_time': self.params.anneal_time / 1000,
+                'auto_scale': False
+            })
+
+        if self.params.use_shim:
+            shim_stats = {'qubit_magnetizations': [],
+                          'average_absolute_value_of_magnetization': [],
+                          'all_flux_bias_offsets': []}
+            sampler_kwargs.update({'readout_thermalization': 100.,
+                                   'auto_scale': False,
+                                   'flux_drift_compensation': True,
+                                   'flux_biases': [0] * base_sampler.properties['num_qubits']})
+            shim_iterations = self.params.shim_iterations
+        else:
+            shim_iterations = 1    # if we don't shim, just run through shim step only once
+
+        # **********************************************************
+        # Step 0: convert game_state to the desired base Ising model
+        # **********************************************************
+
+        # for tangled, h_j=0 for all vertices j in the game graph, and J_ij is one of +1, -1, or 0 for all vertex
+        # pairs i,j. I named the "base" values (the actual problem defined on the game graph we are asked to solve)
+        # base_ising_model.
+
+        base_ising_model = self._game_state_to_ising(game_state)
+
+        # this finds any isolated vertices that may be in the graph -- we will replace the samples returned for these
+        # at the end with true 50/50 statistics, so we don't have to worry about them
+
+        isolated_vertices = self._find_isolated_vertices(game_state)
+
+        # We now enter a loop where each pass through the loop programs the chip to specific values of h and J but
+        # now for the entire chip. We do this by first selecting one automorphism and embedding it in multiple
+        # parallel ways across the entire chip, and then optionally applying a gauge transform across all the qubits
+        # used. This latter process chooses different random gauges for each of the embedded instances.
+
         for _ in range(self.params.num_chip_runs):
-            # Select random automorphism
+
+            # *******************************************************************
+            # Step 1: Randomly select an automorphism and embed it multiple times
+            # *******************************************************************
+
             automorphism = np.random.choice(self.automorphisms)
-            embedding_map = self._process_embedding(game_state, automorphism)
-            
+            embedding_map = self._process_embedding(game_state, automorphism, num_embeddings)
+
+            # *****************************************************************************************************
+            # Step 2: Set h, J parameters for full chip using parallel embeddings of a randomly chosen automorphism
+            # *****************************************************************************************************
+
+            # compute full_h and full_j which are h, jay values for the entire chip assuming the above automorphism
+            # I am calling the problem definition and variable ordering before the automorphism the BLACK or BASE
+            # situation. After the automorphism the problem definition and variable labels change -- I'm calling the
+            # situation after the automorphism has been applied the BLUE situation.
+
+            full_h = {}
+            full_j = {}
+
+            for embedding_idx in range(num_embeddings):
+                for each_vertex in range(num_vertices):
+                    full_h[num_vertices * embedding_idx + each_vertex] = 0
+
+            for k, v in base_ising_model['j'].items():   # is this correct?
+                edge_under_automorph = (min(automorphism[k[0]], automorphism[k[1]]),
+                                        max(automorphism[k[0]], automorphism[k[1]]))
+                full_j[edge_under_automorph] = v
+                for j in range(1, num_embeddings):
+                    full_j[(edge_under_automorph[0] + num_vertices * j,
+                            edge_under_automorph[1] + num_vertices * j)] = v
+
+            # **************************************************************************
+            # Step 3: Choose random gauge, modify h, J parameters for full chip using it
+            # **************************************************************************
+
+            # next we optionally apply a random gauge transformation. I call the situation after the gauge
+            # transformation has been applied the BLUE with RED STAR situation.
+
+            if self.params.use_gauge_transform:
+                flip_map = [random.choice([-1, 1]) for _ in full_h]   # random list of +1, -1 values of len # qubits
+                indices_of_flips = [i for i, x in enumerate(flip_map) if x == -1]       # the indices of the -1 values
+
+                for edge_key, j_val in full_j.items():              # for each edge and associated J value
+                    full_j[edge_key] = j_val * flip_map[edge_key[0]] * flip_map[edge_key[1]]   # Jij -> J_ij g_i g_j
+
+            # *****************************************
+            # Step 4: Choose sampler and its parameters
+            # *****************************************
+
+            sampler_kwargs.update({'h': full_h,
+                                   'J': full_j})
+
+            sampler = FixedEmbeddingComposite(base_sampler, embedding=embedding_map)   # applies the embedding
+
+            # *************************************************************************
+            # Step 5: Optionally start shimming process in the BLUE with RED STAR basis
+            # *************************************************************************
+
+            # all of this in the BLUE with RED STAR basis, ie post automorph, post gauge transform
+            for shim_iteration_idx in range(shim_iterations):
+
+                # **************************************
+                # Step 6: Generate samples from hardware
+                # **************************************
+
+                ss = sampler.sample_ising(**sampler_kwargs)
+                all_samples = ss.record.sample
+
+                if self.params.use_shim:
+
+                    # *************************************************************
+                    # Step 6a: Compute average values of each qubit == magnetization
+                    # *************************************************************
+
+                    magnetization = np.sum(all_samples, axis=0)/self.params.NUM_READS_QC   # BLUE with RED STAR label ordering
+                    shim_stats['average_absolute_value_of_magnetization'].append(np.sum([abs(k) for k in magnetization])/len(magnetization))
+
+                    qubit_magnetization = [0] * base_sampler.properties['num_qubits']
+                    for k, v in embedding_map.items():
+                        qubit_magnetization[v[0]] = magnetization[k]        # check
+
+                    shim_stats['qubit_magnetizations'].append(qubit_magnetization)
+
+                    # **************************************
+                    # Step 6b: Adjust flux bias offset terms
+                    # **************************************
+
+                    for k in range(base_sampler.properties['num_qubits']):
+                        sampler_kwargs['flux_biases'][k] -= self.params.ALPHA_PHI * qubit_magnetization[k]
+
+                    shim_stats['all_flux_bias_offsets'].append(sampler_kwargs['flux_biases'])
+
+            # *****************************************************************************************************
+            # Step 7: Reverse gauge transform, from BLUE with RED STAR to just BLUE, after shimming process is done
+            # *****************************************************************************************************
+
+            if self.params.use_gauge_transform:
+                all_samples[:, indices_of_flips] = -all_samples[:, indices_of_flips]
+
+            # ***********************************
+            # Step 8: Stack samples in BLUE order
+            # ***********************************
+
+            # this should make a big fat stack of the results in BLUE variable ordering
+            all_samples_processed_blue = all_samples[:, range(num_vertices)]
+            for k in range(1, num_embeddings):
+                all_samples_processed_blue = np.vstack((all_samples_processed_blue,
+                                                        all_samples[:, range(num_vertices * k,
+                                                                             num_vertices * (k + 1))]))
+
+            # **********************************************************************
+            # Step 9: Reorder columns to make them BLACK order instead of BLUE order
+            # **********************************************************************
+
+            all_samples_processed_black = all_samples_processed_blue[:, [automorphism[i] for i in range(all_samples_processed_blue.shape[1])]]
+
+            # *********************************************************
+            # Step 10: Add new samples to the stack, all in BLACK order
+            # *********************************************************
+
+            total_samples = np.vstack((total_samples, all_samples_processed_black))
+
+        # ***************************************************************
+        # Step 11: Post process samples stack to extract return variables
+        # ***************************************************************
+
+        total_samples = np.delete(total_samples, (0), axis=0)  # delete first row of zeros
+
+        # replace columns where there are disconnected variables with truly random samples
+        for idx in isolated_vertices:
+            total_samples[:, idx] = np.random.choice([1, -1], size=total_samples.shape[0])
+
+        sample_count = self.params.NUM_READS_QC * num_embeddings * self.params.NUMBER_OF_CHIP_RUNS
+
+        # this is a full matrix with zeros on the diagonal that uses all the samples
+        correlation_matrix = \
+            (np.einsum('si,sj->ij', total_samples, total_samples) / sample_count -
+             np.eye(int(game_state['num_nodes'])))
+
+        winner, score_difference, influence_vector = (
+            self.compute_winner_score_and_influence_from_correlation_matrix(game_state, correlation_matrix))
+
+        # todo make this compatible with output of erik's version
+        return_dictionary = {'game_state': game_state, 'adjudicator': 'quantum_annealing',
+                             'winner': winner, 'score': score_difference, 'influence_vector': influence_vector,
+                             'correlation_matrix': correlation_matrix, 'parameters': self.params}
+
+        return return_dictionary
+
+
+
             # Create sampler with fixed embedding
             sampler = FixedEmbeddingComposite(self._base_sampler, embedding=embedding_map)
             
