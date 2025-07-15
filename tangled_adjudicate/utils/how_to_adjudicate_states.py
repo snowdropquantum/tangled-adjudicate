993756df7b5c4c9afcdd015c17e933ea6e1bf366
Geordie Rose <geordie@snowdropquantum.com>
2025-07-15 12:59:37 -0700

    shows how to use solvers to adjudicate graphs in Phase 1 paper

--- tangled_adjudicate/utils/how_to_adjudicate_states.py
+++ tangled_adjudicate/utils/how_to_adjudicate_states.py
@@ -4,6 +4,8 @@ import os
 import time
 import numpy as np
 
+from tangled_adjudicate.utils.game_graph_properties import GraphProperties
+
 from tangled_adjudicate.adjudicators.simulated_annealing import SimulatedAnnealingAdjudicator
 from tangled_adjudicate.adjudicators.quantum_annealing import QuantumAnnealingAdjudicator
 from tangled_adjudicate.adjudicators.lookup_table import LookupTableAdjudicator
@@ -12,43 +14,60 @@ from tangled_adjudicate.adjudicators.schrodinger import SchrodingerEquationAdjud
 
 def main():
     # this code shows how to use the four different adjudicators
-    # there are two example_game_state dictionaries provided, which are terminal states in graph_number 2 and 3
-    # respectively, that are of the sort that are closest to the draw line at score = +- 1/2
 
     # set graph_number
-    graph_number = 2
+    graph_number = 11   # 11 is P_3, 2 is K_3, 20 is diamond graph, 19 is barbell graph
 
-    # choose solvers to use
-    solver_list = ['simulated_annealing', 'schrodinger_equation', 'quantum_annealing', 'lookup_table']
+    # get graph properties
+    graph = GraphProperties(graph_number=graph_number)
+
+    # choose solver(s) to use
+    # solver_list = ['simulated_annealing', 'schrodinger_equation', 'lookup_table', 'quantum_annealing']
+    solver_list = ['simulated_annealing', 'schrodinger_equation', 'lookup_table']
 
     precision_digits = 4    # just to clean up print output
     np.set_printoptions(suppress=True)   # remove scientific notation
 
     args = {'data_dir': os.path.join(os.getcwd(), '..', 'data'),
-            'graph_number': graph_number}
-
+            'graph_number': graph_number,
+            'lookup_args': {'solver': 'quantum_annealing',   # one of 'simulated_annealing', 'schrodinger_equation', 'quantum_annealing'; must have already been created to use this solver
+                            'epsilon': 0.5,
+                            'anneal_time': 350,
+                            'num_reads': 100000}}
     example_game_state = None
 
-    # draw; score=0; ferromagnetic ring
-    if graph_number == 2:
-        example_game_state = {'num_nodes': 3, 'edges': [(0, 1, 2), (0, 2, 2), (1, 2, 2)],
-                              'player1_id': 'player1', 'player2_id': 'player2', 'turn_count': 5,
-                              'current_player_index': 1, 'player1_node': 1, 'player2_node': 2}
-    else:
-        # red wins, score +2/3; this is one of the states closest to the draw line
-        # note that quantum_annealing in this default uses the D-Wave mock software solver and won't give
-        # the right answer as its samples aren't unbiased -- if you want the quantum_annealing solver to
-        # run on hardware set QAParameters.use_mock = False in /adjudicators/quantum_annealing.py and ensure you have
-        # hardware access and everything is set up
+    # these are in the order introduced in the Phase 1 paper; yes, I realize my numbering scheme is ludicrous
 
-        if graph_number == 3:
-            example_game_state = {'num_nodes': 4, 'edges': [(0, 1, 3), (0, 2, 1), (0, 3, 3),
-                                                            (1, 2, 1), (1, 3, 3), (2, 3, 1)],
-                                  'player1_id': 'player1', 'player2_id': 'player2', 'turn_count': 8,
-                                  'current_player_index': 2, 'player1_node': 2, 'player2_node': 3}
-        else:
-            print('this introduction only has included game states for graphs 2 and 3. If you want a different'
-                  'graph please add a new example_game_state here!')
+    if graph_number == 11:  # P_3 graph, S = [2,3], score = +2 (red)
+        example_game_state = {'num_nodes': 3, 'edges': [(0, 1, 2), (1, 2, 3)],
+                              'player1_id': 'player1', 'player2_id': 'player2', 'turn_count': 4,
+                              'current_player_index': 1, 'player1_node': graph.vertex_ownership[graph_number][0],
+                              'player2_node': graph.vertex_ownership[graph_number][1]}
+
+    if graph_number == 2:   # K_3 graph, S = [3,3,3], score = 0 (draw)
+        example_game_state = {'num_nodes': 3, 'edges': [(0, 1, 3), (0, 2, 3), (1, 2, 3)],
+                              'player1_id': 'player1', 'player2_id': 'player2', 'turn_count': 5,
+                              'current_player_index': 1, 'player1_node': graph.vertex_ownership[graph_number][0],
+                              'player2_node': graph.vertex_ownership[graph_number][1]}
+
+    if graph_number == 20:  # Diamond graph; S = [1,2,2,2,3]; SA scores +4/3, QA & SE scores = +2 (red)
+        example_game_state = {'num_nodes': 4, 'edges': [(0, 1, 1), (0, 3, 2),
+                                                        (1, 2, 2), (1, 3, 2), (2, 3, 3)],
+                              'player1_id': 'player1', 'player2_id': 'player2', 'turn_count': 8,
+                              'current_player_index': 2, 'player1_node': graph.vertex_ownership[graph_number][0],
+                              'player2_node': graph.vertex_ownership[graph_number][1]}
+
+    if graph_number == 19:   # Barbell graph; S =[3,2,2,3,2,3,2]; SA scores -4/9, QA & SE scores +1/2
+        example_game_state = {'num_nodes': 6, 'edges': [(0, 1, 3), (0, 2, 2), (1, 2, 2),
+                                                        (2, 5, 3), (3, 4, 2), (3, 5, 3),
+                                                        (4, 5, 2)],
+                              'player1_id': 'player1', 'player2_id': 'player2', 'turn_count': 9,
+                              'current_player_index': 1, 'player1_node': graph.vertex_ownership[graph_number][0],
+                              'player2_node': graph.vertex_ownership[graph_number][1]}
+
+    if example_game_state is None:
+        sys.exit(print('this introduction only includes game states for graphs 2, 11, 19, and 20. '
+                       'If you want a different graph please add a new example_game_state here!'))
 
     for solver_to_use in solver_list:
 
